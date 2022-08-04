6e9e78f1de91f621de92f822a486799fc280a0e7
sultanby <jemjasott@gmail.com>
2022-08-04 21:09:55 +0200

    react router prototype done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import './App.css';
+import { Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
+    <Router>
+      <Header />
+      <Switch>
+        <Route path={'/page-not-found'}>
+          <PageNotFound />
+        </Route>
+        <Route path={'/search'}>
+          <SearchPage />
+        </Route>
+        <Route path={'/r/:subreddit/comments/:id/:title'}>
+          <Post />
+        </Route>
+        <Route path={'/r/:subreddit/:filter'}>
+          <Feed />
+        </Route>
+        <Route path={'/:filter'}>
+          <Feed />
+        </Route>
+        <Route path={'/'}>
+          <Feed />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
