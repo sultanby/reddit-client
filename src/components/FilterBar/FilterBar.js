842c5de7246c09a67d8704adf77df85bc578325f
sultanby <jemjasott@gmail.com>
2022-08-18 11:42:40 +0200

    FilterBar icons added

--- src/components/FilterBar/FilterBar.js
+++ src/components/FilterBar/FilterBar.js
@@ -1,9 +1,29 @@
 import React from "react";
 import { Link, useRouteMatch, useParams } from "react-router-dom";
 import "./FilterBar.css";
+import hotIcon from '../../assets/flame-24.png';
+import newIcon from '../../assets/new-24.png';
+import topIcon from '../../assets/top-24.png';
+
+
 
 function FilterBar() {
-  const filters = ["hot", "new", "top"];
+  const filters = [
+    {
+      'index': 1,
+      'type': 'hot',
+      'src': hotIcon
+    }, 
+    {
+      'index': 2,
+      'type': 'new',
+      'src': newIcon
+    },
+    {
+      'index': 3,
+      'type': 'top',
+      'src': topIcon
+    }];
   let { path, url } = useRouteMatch();
   //console.log(url + "    " + path);
   const params = useParams();
@@ -12,12 +32,12 @@ function FilterBar() {
 
   return (
     <div className="filter-bar">
-      {filters.map((filter, index) => {
+      {filters.map((filter) => {
         if (params.subreddit && path==="/r/:subreddit/") 
-          return <Link to={`${url}/${filter}`} key={index}><button>{filter}</button></Link>
+          return <Link to={`${url}/${filter.type}`} key={filter.index}><button><img src={filter.src} alt='filter icon'/>{filter.type}</button></Link>
         else if (Object.keys(params).length === 2)
-            return <Link to={`/${url_help}/${filter}`} key={index}><button>{filter}</button></Link>
-        return <Link to={`/${filter}`} key={index}><button>{filter}</button></Link>
+            return <Link to={`/${url_help}/${filter.type}`} key={filter.index}><button><img src={filter.src} alt='filter icon'/>{filter.type}</button></Link>
+        return <Link to={`/${filter.type}`} key={filter.index}><button><img src={filter.src} alt='filter icon'/>{filter.type}</button></Link>
     })}
     </div>
   );
