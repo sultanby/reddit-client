2685390d729750f9ad08de922543f76f74c6a891
sultanby <jemjasott@gmail.com>
2022-08-21 18:45:08 +0200

    scroll-to-top added

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import Header from "./components/Header/Header";
 import Feed from "./components/Feed/Feed";
 import Comments from "./components/Comments/Comments";
 import PageNotFound from "./components/PageNotFound/PageNotFound";
-import { Redirect } from "react-router-dom";
+import ScrollToTop from "react-scroll-to-top";
 
 function App() {
   return (
     <>
+    <ScrollToTop />
       <Header />
       <div id="main">
         <Switch>
